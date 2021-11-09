88924f237031d1e35d7c38249ffc5bfb915935e3
Marco Schlicht <git@privacymail.dev>
2021-11-09 15:57:13 +0100

    improved functions

--- src/nodes.py
+++ src/nodes.py
@@ -55,11 +55,15 @@ class FunctionNode(Node):
         self.function_name = function_name
         self.code_block = code_block
 
+    def loc(self):
+        return self.code_block.loc() + 4
+
     def __repr__(self):
         out = ""
-        lines = self.code_block.loc() + 1
-        out += "op add fn_move @counter 1\n"
+        lines = self.code_block.loc() + 2
+        out += f"op add {self.function_name} @counter 1\n"
         out += f"op add @counter @counter {lines}\n"
+        out += f"set _{self.function_name}_retptr @counter\n"
         out += str(self.code_block)
         out += "set @counter retptr"
         return out
@@ -74,3 +78,16 @@ class CommentNode(Node):
 
     def loc(self):
         return 0
+
+
+class ExecNode(Node):
+    def __init__(self, fnptr):
+        self.fnptr = fnptr
+
+    def loc(self):
+        return 2
+
+    def __repr__(self):
+        out = "op add retptr @counter 1\n"
+        out += f"set @counter {self.fnptr}"
+        return out
