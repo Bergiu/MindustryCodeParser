283650e7312cb8d77dd15a01e8980d9294d8181e
Marco Schlicht <git@privacymail.dev>
2021-11-09 11:14:18 +0100

    load code from path

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,13 @@
-def load_code(filename: str) -> str:
+import pathlib
+from typing import Union
+
+
+def load_code(filename: Union[str, pathlib.Path]) -> str:
     with open(filename, "r") as f:
         lines = f.read()
     return lines
 
 
-def write_code(filename: str, text: str):
+def write_code(filename: Union[str, pathlib.Path], text: str):
     with open(filename, "w") as f:
         f.write(text)
